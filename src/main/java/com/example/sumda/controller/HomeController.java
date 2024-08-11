6c2c7f4739522613fe68e82596eef2e626bef2ce
dongmin132 <m914@naver.com>
2024-08-11 20:01:46 +0900

    first commit

---
+++ src/main/java/com/example/sumda/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.sumda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Spring Boot!";
+    }
+}
