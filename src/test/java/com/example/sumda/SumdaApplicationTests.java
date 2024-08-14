915a4b758ef7cee2125b3cd67a92198c290195df
dongmin132 <m914@naver.com>
2024-08-14 23:16:09 +0900

    first setting

---
+++ src/test/java/com/example/sumda/SumdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sumda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
