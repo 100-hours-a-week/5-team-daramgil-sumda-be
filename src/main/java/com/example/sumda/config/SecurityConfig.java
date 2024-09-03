435b2b460e7108a9ee3b1925680421dbfdf3fbac
dongmin132 <m914@naver.com>
2024-09-03 20:22:40 +0900

    feat: 네이버 아이디 로그인

--- src/main/java/com/example/sumda/config/SecurityConfig.java
+++ src/main/java/com/example/sumda/config/SecurityConfig.java
@@ -1,31 +1,32 @@
-//package com.example.sumda.config;
-//
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/login", "/register").permitAll()  // 로그인 및 등록 페이지는 인증 필요 없음
-//                .anyRequest().authenticated()  // 다른 모든 요청은 인증 필요
-//                .and()
-//                .formLogin()
-//                .loginPage("/login")  // 커스텀 로그인 페이지 설정
-//                .permitAll()
-//                .and()
-//                .logout()
-//                .permitAll();
-//
-//        // 세션 관리 추가
-//        http
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)  // 세션이 필요할 때만 생성
-//                .maximumSessions(1);  // 최대 1개의 세션 허용 (사용자당)
-//    }
-//}
+package com.example.sumda.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/img/**","/images/**").permitAll()  // 이미지 경로 허용
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement((sessionManagement) -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
