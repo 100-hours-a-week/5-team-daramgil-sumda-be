756a07e69fefe359692419eb5e21e22057681dec
dongmin132 <m914@naver.com>
2024-09-11 00:31:22 +0900

    fix: refresh token rdb 변경 완료

--- src/main/java/com/example/sumda/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/sumda/repository/RefreshTokenRepository.java
@@ -1,9 +1,10 @@
 package com.example.sumda.repository;
 
 import com.example.sumda.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 }
