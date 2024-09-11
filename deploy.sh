a65101c09ce5729082a95b198970d129a02aa505
KGY <eakxm1234@naver.com>
2024-09-11 21:41:24 +0900

    최종 수정

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ EXIST_BLUE=$(docker-compose -f /home/ubuntu/docker-compose.blue.yml ps -q spring
 echo "EXIST_BLUE 값: ${EXIST_BLUE}"
 
 if [ -z "${EXIST_BLUE}" ]; then
-  # blue가 실행 중이지 않으면 blue 컨테이너를 시작
+  # green이 실행 중이면 blue 컨테이너를 시작
   docker-compose -f /home/ubuntu/docker-compose.blue.yml up -d spring-backend
   START_CONTAINER="blue"
   TERMINATE_CONTAINER="green"
@@ -33,7 +33,7 @@ RUNNING_CONTAINER=$(sudo docker ps)
 echo "실행중인 컨테이너 목록: ${RUNNING_CONTAINER}"
 
 # 서버 상태 확인
-for cnt in {1..10}; do
+for cnt in {1..20}; do
     echo "서버 상태 확인 중..."
 
     UP=$(curl -s http://127.0.0.1:${START_PORT}/api/health | grep 'UP')
@@ -46,10 +46,17 @@ for cnt in {1..10}; do
     sleep 10
 done
 
-if [ $cnt -eq 10 ]; then
+if [ $cnt -eq 20 ]; then
     echo "배포 실패: 서버가 시작되지 않았습니다."
     exit 1
 fi
 
 # NGINX 포트 변경 및 재시작
 echo "NGINX 설정 변경 및 재시작..."
+sudo cp /etc/nginx/nginx.blue.conf /etc/nginx/nginx.green.conf
+sudo service nginx reload
+
+#이전 컨테이너 중지
+docker-compose -f /home/ubuntu/docker-compose.${TERMINATE_CONTAINER}.yml down
+echo "${TERMINATE_CONTAINER} 컨테이너를 중지했습니다."
+echo "배포 완료"
