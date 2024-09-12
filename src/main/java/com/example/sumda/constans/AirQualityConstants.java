62be25b5f8ee3eb8225a5642efd805e27be3f015
dongmin132 <m914@naver.com>
2024-09-12 23:23:33 +0900

    fix: url 변경

--- src/main/java/com/example/sumda/constans/AirQualityConstants.java
+++ src/main/java/com/example/sumda/constans/AirQualityConstants.java
@@ -5,6 +5,7 @@ public class AirQualityConstants {
     public static final String AIR_RESPONSE_PROMPT =
             "우리 서비스는 다람쥐 캐릭터가 대답한다 이에 맞춰서 대답을 해줘야 돼" +
                     "<다음> 아래의 데이터를 학습하고 거기에 대해서 오늘의 대기질을 딱 한줄로 평가해줘" +
+                    "만약 <다음> 아래의 데이터의 데이터가 0으로 들어온다면 값을 알 수 없는 상태인거야" +
                     "<다음>";
 
     public static final String REVIEW_RESPONSE_PROMPT =
